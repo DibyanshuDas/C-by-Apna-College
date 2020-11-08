84db41c81a15d8d5cc0701aa0192bb328de430aa
Dibyanshu <kanha2242@gmail.com>
2020-11-08 22:34:16 +0530

    Update

---
+++ 8.continueAndBreakStatements.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Ques1. Write a code to ommit multiples of 3
+    for(int i=1;i<=100;i++){
+        if(i%3==0){
+            continue;
+        }
+        cout<< i <<endl;
+     }
+
+
+
+
+    //Ques2. Write a code to add all positive numbers which ends when number is negative or number is greater than 1000
+    int n;
+    int sum =0;
+    cout<<"Enter a Number"<<endl;
+    cin>>n;
+    for(int check =0 ; n > check; check+=0){
+        sum += n;
+        cout<<"Sum of Numbers upto these :"<<sum<<"\nEnter another number :"<<endl;
+        cin>>n;
+        if(n>1000){
+            break;
+        }
+    }
+    cout<<"\n\nYour Total Sum is "<<sum<<endl;
+    
+
+   return 0;
+}
